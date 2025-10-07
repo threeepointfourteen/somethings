ea2da6b02b14946f603a7c3d8f48ee3401b2a9f0
threeepointfourteen <somosdelclique@gmail.com>
2025-10-06 20:48:31 -0500

    Configurar GitHub Pages con GitHub Actions para Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://threeepointfourteen.github.io',
+  base: '/somethings',
   vite: {
     plugins: [tailwindcss()]
   }
